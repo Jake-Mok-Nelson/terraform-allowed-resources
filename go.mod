b82ac31cc067b6d9d6f99ac755178026f511b9b0
Jake-Mok-Nelson <jakesmnelson@gmail.com>
2022-06-23 13:31:32 +1000

    Add placeholder for Kong CLI parser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Jake-Mok-Nelson/terraform-allowed-resources
 
 go 1.18
+
+require github.com/alecthomas/kong v0.6.1
--- src/main.go
+++ src/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "github.com/Jake-Mok-Nelson/terraform-allowed-resources/src/placeholder"
+import (
+	"fmt"
+
+	"github.com/Jake-Mok-Nelson/terraform-allowed-resources/src/placeholder"
+	"github.com/alecthomas/kong"
+)
+
+type Context struct {
+	Debug bool
+}
+
+type EvalCmd struct {
+	RulesFile string `help:"Path to the file containing your rules." arg type:"existingfile" default:"rules.json" name:"rules"`
+	PlanFile  string `help:"Path to the Terraform plan file." arg type:"existingfile" default:"plan.tfplan" name:"plan"`
+	Debug     bool   `help:"Enable debug mode." default:"false"`
+}
+
+func (r *EvalCmd) Run(ctx *Context) error {
+	fmt.Println("eval", r.RulesFile, r.PlanFile)
+	placeholder.PlaceHolder()
+	return nil
+}
+
+var cli struct {
+	Eval EvalCmd `cmd help:"Evaluate the Terraform Plan against provided rules."`
+}
 
 func main() {
-	placeholder.PlaceHolder()
+
+	ctx := kong.Parse(&cli)
+	err := ctx.Run(&Context{})
+	ctx.FatalIfErrorf(err)
 }
